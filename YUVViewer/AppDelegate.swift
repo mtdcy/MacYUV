4df5c7a5c6808d551a0df77053bc336b5312679e
mtdcy.chen <mtdcy.chen@gmail.com>
2019-04-14 19:11:05 +0800

    Initial Commit

---
+++ YUVViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  YUVViewer
+//
+//  Created by Chen Fang on 2019/4/14.
+//  Copyright © 2019 Chen Fang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ YUVViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  YUVViewer
+//
+//  Created by Chen Fang on 2019/4/14.
+//  Copyright © 2019 Chen Fang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
